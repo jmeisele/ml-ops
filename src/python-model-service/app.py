fd32e58cc736219631f1510bbcefc709c0d0a4df
jmeisele <jmeisele@yahoo.com>
2020-05-22 04:15:43 -0500

    :rocket: compose finished

--- src/python-model-service/app.py
+++ src/python-model-service/app.py
@@ -32,4 +32,4 @@ def index():
 #     return jsonify(results=output)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8088, debug=True)
+    app.run(host='0.0.0.0', port=8000, debug=True)
