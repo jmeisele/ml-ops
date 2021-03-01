867b912a98246a371736d8684194dd709325c495
jmeisele <jmeisele@yahoo.com>
2021-03-01 12:29:26 -0600

    :construction: bridge server done, logging request body currently

--- bridge_server/app.py
+++ bridge_server/app.py
@@ -1,7 +1,9 @@
 import json
-from requests import request
+import requests
+
 from dag_mapping import config_map
 
+from loguru import logger
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
@@ -9,11 +11,15 @@ app = Flask(__name__)
 
 @app.route("/route", methods=["POST"])
 def reroute():
-    url = "http://localhost:8080/api/dags/example_bash_operator/dagRuns"
+    # Decode the request body
+    body = request.data.decode("utf-8")
+    logger.debug(body)
+    url = "http://airflow-webserver:8080/api/dags/example_bash_operator/dagRuns"
     payload = {"conf": {}}
-    headers = 
-    response = request.post(url=url, data=json.dumps(payload), headers=headers)
-    data = response.json()
+    # headers = 
+    # response = requests.request.post(url=url, data=json.dumps(payload), headers=headers)
+    # data = response.json()
+    return "Got it!"
 
 
 @app.route("/health", methods=["GET"])
@@ -22,4 +28,4 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=True)
+    app.run(host="0.0.0.0", port=8002, debug=True)
