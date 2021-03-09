a6b477c9e287d0780056a1e0a675666a4969e7ee
jmeisele <jmeisele@yahoo.com>
2021-03-09 08:26:29 -0600

    :construction: prometheus scraping metrics from fastapi and rabbitmq

--- send_data.py
+++ send_data.py
@@ -15,15 +15,15 @@ for i in range(len(data.data)):
     array = data.data[i]
     target = data.target[i]
     body = {"median_income_in_block": array[0],
-            "median_house_age_in_block": array[1],
-            "average_rooms": array[2],
-            "average_bedrooms": array[3],
-            "population_per_block": array[4],
+            "median_house_age_in_block": int(round(array[1])),
+            "average_rooms": int(round(array[2])),
+            "average_bedrooms": int(round(array[3])),
+            "population_per_block": int(round(array[4])),
             "average_house_occupancy": array[5],
             "block_latitude": array[6],
             "block_longitude": array[7]
             }
     # POST request to our API
-    requests.post(url="http://localhost/api/model/predict", json=body)
+    requests.post(url="http://localhost/model/predict", json=body)
     # requests.post(url="http://localhost:8002/route", json=body)
     time.sleep(1)
